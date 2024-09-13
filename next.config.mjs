a39f96fde72e5bbbf021d0c802c413e0ceebac26
einslen <valentin.damlencour@gmail.com>
2024-09-13 15:05:04 +0200

    add ci

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@ import withBundleAnalyzer from '@next/bundle-analyzer';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    basePath: '/portfolio-valentin', // Le nom de ton repo GitHub
+    assetPrefix: '/portfolio-valentin/', // Préfixe pour les assets
     experimental: {
         optimizePackageImports: ['icon-library'],
         optimizeCss: true,
