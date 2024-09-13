14089ba308b28c3477718dfd22e41e53dd96e055
DVLaD <valentin.damlencour@gmail.com>
2024-09-13 23:22:32 +0200

    test export

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/portfolio-valentin', // Le nom de ton repo GitHub
-    assetPrefix: '/portfolio-valentin/', // Préfixe pour les assets
     experimental: {
         optimizePackageImports: ['icon-library'],
         optimizeCss: true,
