abc7474b9d59bcf48e458584955c9eeb8a922043
DVLaD <valentin.damlencour@gmail.com>
2024-09-14 01:55:10 +0200

    add swc monify

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'out',
+    basePath: '/portfolio', // Utiliser le sous-répertoire
+    assetPrefix: 'https://einslen.github.io/portfolio/',
     experimental: {
         optimizePackageImports: ['icon-library'],
         optimizeCss: true,
         optimisticClientCache: true,
         optimizeServerReact: true
     },
+    swcMinify: true,
 };
 
 export default nextConfig;
