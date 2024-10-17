69960921728b8bb3845d37b0d99a652d56d550f1
DVLaD <valentin.damlencour@gmail.com>
2024-10-17 19:20:37 +0200

    add title in timeline

--- src/features/portfolio/components/competence/competence.tsx
+++ src/features/portfolio/components/competence/competence.tsx
@@ -1,10 +1,14 @@
 export const Competence = () => {
     return (
-        <div className="relative h-20 ">
+        <div className="relative h-32"> {/* Augmenté la hauteur pour faire de la place au h1 */}
+            {/* Titre au-dessus de la banderole */}
+            <h1 className="text-2xl -rotate-6 font-bold text-center mb-4 text-gray-900 dark:text-white">Mes compétences</h1>
+
+            {/* Banderole avec les compétences */}
             <div
                 className="absolute -rotate-6 bg-gradient-to-r from-green-400 to-blue-500 text-white w-[150vw] left-[-25vw] py-3">
                 <p className="text-center uppercase tracking-widest text-lg whitespace-nowrap animate-scroll-mobile md:animate-none">
-                    ★ React/Next ★ Html/CSS ★ PHP/Laravel/symfony  ★ Python/Flask ★ SQL/MongoDB/Postgresql ★ Java ★ IntellIJ ★
+                    ★ React/Next ★ Html/CSS ★ PHP/Laravel/symfony ★ Python/Flask ★ SQL/MongoDB/Postgresql ★ Java ★ IntellIJ ★
                 </p>
             </div>
         </div>
