eebd1a6639951eb213339ed75d502b566f104ac9
valentin_damlencourt <valentin.damlencourt@gmail.com>
2024-10-17 16:43:37 +0200

    mobile competence

--- src/features/portfolio/components/competence/competence.tsx
+++ src/features/portfolio/components/competence/competence.tsx
@@ -1,10 +1,9 @@
 export const Competence = () => {
     return (
         <div className="relative h-20 pt-16 pb-[calc(100vh-750px)] md:pt-[calc(100vh-550px)] md:pb-[calc(100vh-450px)] overflow-hidden">
-            <div
-                className="absolute -rotate-6 bg-gradient-to-r from-green-400 to-blue-500 text-white w-[150vw] left-[-25vw] py-3">
-                <p className="text-center uppercase tracking-widest text-lg">
-                    ★ React/Next ★ Html/CSS ★ PHP/Laravel/symfony  ★ Python/Flask ★ SQL/MongoDB/Postgresql ★ Java ★ IntellIJ ★
+            <div className="absolute -rotate-6 bg-gradient-to-r from-green-400 to-blue-500 text-white w-[150vw] left-[-25vw] py-3">
+                <p className="text-center uppercase tracking-widest text-lg whitespace-nowrap animate-scroll-mobile md:animate-none">
+                    ★ React/Next ★ Html/CSS ★ PHP/Laravel/Symfony ★ Python/Flask ★ SQL/MongoDB/Postgresql ★ Java ★ IntelliJ ★
                 </p>
             </div>
         </div>
