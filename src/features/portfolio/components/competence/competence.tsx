4861e48696e9d42c928534511b4dc144bb4173aa
DVLaD <valentin.damlencour@gmail.com>
2024-10-19 01:10:36 +0200

    add font bold

--- src/features/portfolio/components/competence/competence.tsx
+++ src/features/portfolio/components/competence/competence.tsx
@@ -9,7 +9,7 @@ const Competence = () => {
             {/* Banderole avec les compétences */}
             <div
                 className="absolute -rotate-6 bg-gradient-to-r from-green-400 to-blue-500 text-white w-[150vw] left-[-25vw] py-3">
-                <p className="text-center uppercase tracking-widest text-lg whitespace-nowrap animate-scroll-mobile md:animate-none">
+                <p className="text-center font-bold uppercase tracking-widest text-lg whitespace-nowrap animate-scroll-mobile md:animate-none">
                     ★ React/Next ★ Html/CSS ★ PHP/Laravel/symfony ★ Python/Flask ★ SQL/MongoDB/Postgresql ★ Java ★ IntellIJ ★
                 </p>
             </div>
