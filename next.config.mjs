a206cbf27315a6f3588b9f9e7558e6acc77ad05b
DVLaD <valentin.damlencour@gmail.com>
2024-10-18 19:27:19 +0200

    add print css

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,14 @@ const nextConfig = {
     output: 'out',
     basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
     assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
-
+    compress: true,
+    experimental: {
+        optimizePackageImports: ['icon-library'],
+        optimizeCss: true,
+        optimisticClientCache: true,
+        optimizeServerReact: true
+    },
+    swcMinify: true,
 };
 
 export default nextConfig;
