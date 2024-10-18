52ca8d59193d3f0e1625c17c8ea4724c0e4f15ab
valentin_damlencourt <valentin.damlencourt@gmail.com>
2024-10-18 11:01:32 +0200

    test with img

--- src/features/portfolio/components/projects/projects.tsx
+++ src/features/portfolio/components/projects/projects.tsx
@@ -9,7 +9,7 @@ const projects = [
         techUsed: ['ReactJs', 'Firebase V9', 'React-Icons', 'CSS3', 'HTML'],
         siteLink: '#',
         githubLink: '#',
-        image: `${basePath}/images/projects/project01.webp`, // Remplace par le bon chemin d'image
+        image: `${basePath}/images/projects/Flatcraft.webp`, // Remplace par le bon chemin d'image
     },
     {
         name: 'TEST',
@@ -18,7 +18,7 @@ const projects = [
         techUsed: ['ReactJs', 'Firebase V9', 'React-Icons', 'CSS3', 'HTML'],
         siteLink: '#',
         githubLink: '#',
-        image: `${basePath}/images/projects/project01.webp`, // Remplace par le bon chemin d'image
+        image: `${basePath}/images/projects/Raytracer.webp`, // Remplace par le bon chemin d'image
     },
     {
         name: 'TEST',
@@ -27,7 +27,7 @@ const projects = [
         techUsed: ['ReactJs', 'Firebase V9', 'React-Icons', 'CSS3', 'HTML'],
         siteLink: '#',
         githubLink: '#',
-        image: `${basePath}/images/projects/project01.webp`, // Remplace par le bon chemin d'image
+        image: `${basePath}/images/projects/image.avif`, // Remplace par le bon chemin d'image
     },
     {
         name: 'TEST',
@@ -36,7 +36,7 @@ const projects = [
         techUsed: ['ReactJs', 'Firebase V9', 'React-Icons', 'CSS3', 'HTML'],
         siteLink: '#',
         githubLink: '#',
-        image: `${basePath}/images/projects/project01.webp`, // Remplace par le bon chemin d'image
+        image: `${basePath}/images/projects/pokemon.avif`, // Remplace par le bon chemin d'image
     },
 ];
 
