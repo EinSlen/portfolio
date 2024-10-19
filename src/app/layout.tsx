39948584d0081f4099a8c9639efcc532d1a8f2f2
valentin_damlencourt <valentin.damlencourt@gmail.com>
2024-10-19 15:43:04 +0200

    try children

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body>
       <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
         <Header />
-        <div className="min-h-[calc(100vh-64px)]">{children}</div>
+        <Footer />
       </ThemeProvider>
       </body>
       </html>
