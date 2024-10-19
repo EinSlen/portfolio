d39d8eead12745aac4541eae727dd1fefa5c7e57
valentin_damlencourt <valentin.damlencourt@gmail.com>
2024-10-19 15:46:04 +0200

    check everything

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,27 +6,6 @@ import Loader from "@/components/loader";
 
 //export const revalidate = REVALIDATE_TIME;
 
-export const metadata: Metadata = {
-  title: {
-    default: "Portfolio de Damlencourt Valentin",
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-  icons: {
-    icon: "./favicon.ico",
-  }
-};
-
-// Dynamic imports
-const ThemeProvider = dynamic(() => import("@/components/theme-provider"), {
-  ssr: false,
-});
-const Header = dynamic(() => import("@/components/header/header"), { ssr: false, loading: () => <Loader /> });
-const Footer = dynamic(() => import("@/components/footer/footer"), { ssr: false });
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -36,10 +15,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
       <html lang="fr">
       <body>
-      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-        <Header />
-        <Footer />
-      </ThemeProvider>
+
       </body>
       </html>
   );
