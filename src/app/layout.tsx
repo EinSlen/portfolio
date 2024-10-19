bf3514bd8a1f4ef88ac958737af4144b272fe809
valentin_damlencourt <valentin.damlencourt@gmail.com>
2024-10-19 15:30:53 +0200

    check is it's the theme provider generate the 404

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,11 +36,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
       <html lang="fr">
       <body>
-      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
         <Header />
         <div className="min-h-[calc(100vh-64px)]">{children}</div>
         <Footer />
-      </ThemeProvider>
       </body>
       </html>
   );
