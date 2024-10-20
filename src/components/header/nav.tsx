91dd82daeeb07c95c3e9551449e00f56e1791e91
valentin_damlencourt <valentin.damlencourt@gmail.com>
2024-10-20 14:27:53 +0200

    remove onPath in nav bar

--- src/components/header/nav.tsx
+++ src/components/header/nav.tsx
@@ -1,8 +1,4 @@
-"use client"
-
 import React from "react"
-import Link from "next/link"
-import { usePathname } from "next/navigation"
 import { INavItem } from "@/types"
 import { cn } from "@/utils"
 
@@ -14,7 +10,6 @@ interface NavProps {
 
 export const Nav = (props: NavProps) => {
   const { items, direction = "row", onNavItemClick } = props
-  const pathname = usePathname()
 
   return (
     <nav
@@ -25,20 +20,17 @@ export const Nav = (props: NavProps) => {
       {items?.map(
         (item, index) =>
           item.href && (
-            <Link
+            <a
               key={index}
               href={item.href}
               className={cn(
                 "hover:text-foreground flex items-center justify-center rounded-lg px-5 py-2 text-sm font-medium transition-all",
-                item.disabled && "cursor-not-allowed opacity-80",
-                item.href === pathname
-                  ? "bg-foreground/10 text-foreground"
-                  : "text-foreground/60"
+                item.disabled && "cursor-not-allowed opacity-80", "text-foreground/60"
               )}
               onClick={onNavItemClick}
             >
               {item.title}
-            </Link>
+            </a>
           )
       )}
     </nav>
