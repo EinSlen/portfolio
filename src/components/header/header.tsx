ad894f7d0c72773051df1709a189910e998a94e1
valentin_damlencourt <valentin.damlencourt@gmail.com>
2024-10-20 14:16:20 +0200

    try with / to see if we provide a 404

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -14,7 +14,12 @@ export const Header = () => {
     <header className={cn("sticky top-0 z-40 w-full")}>
       <div className="relative">
         <div className="container flex h-16 items-center px-4 sm:px-8">
-            <span onClick={() => window.location.reload()} className="inline-block font-bold">{"<"+name+"/>"}</span>
+          <a
+            href="/"
+            className="mr-8 flex items-center space-x-2 transition-all hover:opacity-80"
+          >
+            <span className="inline-block font-bold">{"<"+name+"/>"}</span>
+          </a>
           <div className="hidden flex-1 sm:flex">
             <div className="flex-1">
               <Nav items={nav}/>
