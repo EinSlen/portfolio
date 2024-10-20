26f52ebc9f2c8473dc0cb5d5e461fd46d0231a37
DVLaD <valentin.damlencour@gmail.com>
2024-10-20 18:40:23 +0200

    rework the card to be the same as 2 others

--- src/features/portfolio/components/skillsAndLanguage.tsx
+++ src/features/portfolio/components/skillsAndLanguage.tsx
@@ -1,6 +1,6 @@
 const SkillsAndLanguages = () => {
     return (
-        <div className="flex flex-col items-center w-full md:max-w-lg lg:max-w-3xl max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 h-full">
+        <div className="flex flex-col items-center w-full md:max-w-lg lg:max-w-[calc(768px+10px)] max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 h-full">
             <h2 className="text-lg dark:bg-gray-800 dark:border-gray-700 font-bold mb-6">💼 Soft Skills & Langues 🌐</h2>
             <div className="flex flex-col md:flex-row justify-between w-full md:space-x-4 space-y-6 md:space-y-0">
                 <div className="flex-1 flex flex-col items-start">
