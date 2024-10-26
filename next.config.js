2cd73ce5a9bcd0b7c830d78c20a2e51416950197
DVLaD <valentin.damlencour@gmail.com>
2024-10-26 12:20:48 +0200

    fix(build): try to bundle without site variable in .env.production

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compress: true,
-  headers() {
+  async headers() {
     return [
       {
-        source: '/_next/static/(.*)',
+        source: '/_next/static/(.*)', // Cible les fichiers statiques de Next.js
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
+            value: 'public, max-age=31536000, immutable', // Cache pour un an
           },
           {
             key: 'Expires',
-            value: new Date(Date.now() + 31536000000).toUTCString(),
+            value: new Date(Date.now() + 31536000000).toUTCString(), // Expire dans un an
           },
         ],
       },
